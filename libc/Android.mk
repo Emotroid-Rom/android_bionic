13e7126cdcabd178dfd9991f01cd5ea46c552d30
Joe Maples <joe@frap129.org>
2016-04-27 20:40:19 -0400

    Enable Clang for bionic
    Signed-off-by: Joe Maples <joe@frap129.org>

--- libc/Android.mk
+++ libc/Android.mk
@@ -584,7 +584,7 @@ ifneq ($(TARGET_USES_LOGD),false)
 libc_common_cflags += -DTARGET_USES_LOGD
 endif
 
-use_clang := false
+use_clang := true
 
 # Try to catch typical 32-bit assumptions that break with 64-bit pointers.
 libc_common_cflags += \
